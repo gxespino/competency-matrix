a684775eb6b6884b08d9cbb65f7d1dd2b1e98e41
gxespino <glennpeter.espinosa@gmail.com>
2017-09-25 13:17:43 -0400

    Add progressable competencies

--- app/assets/javascripts/matrices.js
+++ app/assets/javascripts/matrices.js
@@ -1,11 +1,19 @@
 function selectCompetency(event) {
   var competency = event.target
 
-  if (competency.classList.contains('selected')) {
-    unselect(competency)
-  } else {
+  if (competency.classList.contains('completed')) {
     clearParentRow(competency)
-    select(competency)
+    unselect(competency, 'completed')
+    select(competency, 'working')
+    setParentRow(competency)
+  } else if (competency.classList.contains('working')) {
+    unselect(competency, 'working')
+    select(competency, 'completed')
+    setParentRow(competency)
+  } else {
+    unselect(competency, 'completed')
+    select(competency, 'working')
+    setParentRow(competency)
   }
 }
 
@@ -13,18 +21,51 @@ function clearParentRow(competency) {
   var parentRowCells = competency.parentElement.cells
 
   for (var i = 0, len = parentRowCells.length; i < len; i++) {
-    unselect(parentRowCells[i]);
+    unselect(parentRowCells[i], 'working');
+    unselect(parentRowCells[i], 'completed');
   }
 }
 
-function select(element) {
-  element.classList.add('selected')
+function setParentRow(competency) {
+  var parentRowCells = competency.parentElement.cells
+  var cellsArray = []
+
+  // Convert parentRowCells object to Array
+  for (var i = 0, len = parentRowCells.length; i < len; i++) {
+    cellsArray.push(parentRowCells[i])
+  }
+
+  for (var i = 0, len = cellsArray.length; i < len; i++) {
+    var competencyIndex = cellsArray.indexOf(competency)
+
+    // Don't set styles for clicked competency
+    if (i === competencyIndex) {
+      return
+    }
+
+    // For cells with index less than competency - set to completed
+    if (i < competencyIndex) {
+      unselect(cellsArray[i], 'working');
+      select(cellsArray[i], 'completed');
+    }
+
+    // For cells with index greater than competency - clear styles
+    if (i > competencyIndex) {
+      unselect(cellsArray[i], 'working');
+      unselect(cellsArray[i], 'completed');
+    }
+  }
 }
 
-function unselect(element) {
-  element.classList.remove('selected')
+function select(element, tdClass) {
+  element.classList.add(tdClass)
 }
 
+function unselect(element, tdClass) {
+  element.classList.remove(tdClass)
+}
+
+// Allow dl, dt, dd & competency-text classes to be clickable
 document.addEventListener("DOMContentLoaded", function(){
   [].forEach.call(document.querySelectorAll('dl', 'dt', 'dd', 'competency-text'), function(el) {
     el.addEventListener('click', function(event) {
